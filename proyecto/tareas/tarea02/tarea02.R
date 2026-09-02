9330d3401074fdf23d5b189b729ca5c883ab039d
Valentin Garcia Perillo <valengelp15@gmail.com>
2026-09-02 17:25:10 -0300

    Tarea 02: R base

---
+++ proyecto/tareas/tarea02/tarea02.R
@@ -0,0 +1,18 @@
+encuestado_id <- 1045
+
+ingreso <- 350000.50
+miembros_hogar <- 4L
+estado <- "Ocupado"
+busca_trabajo <- FALSE
+
+class(ingreso)
+class(busca_trabajo)
+nchar(sector_actividad)
+
+paste("Sector:", sector_actividad, "-", categoria_ocupacional)
+
+grepl("propia", "Cuenta propia con local")
+sector_actividad <-"Comercio"
+categoria_ocupacional <-'cuentapropista'
+paste("Sector:", sector_actividad, "-", categoria_ocupacional)
+nchar(sector_actividad)
